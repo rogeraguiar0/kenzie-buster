edc052ded7b1db20c5ae8473a79ad0bdf1ecde32
rogeraguiar0 <aguiarroger02@gmail.com>
2023-02-13 20:19:52 -0300

    feat: add user custom model and email and username unique verification

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    username = models.CharField(max_length=127, unique=True)
+    email = models.EmailField(max_length=127, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birthdate = models.DateField(null=True)
+    is_employee = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserViews(APIView):
+    def post(self, request: Request) -> Response:
+        serializer = UserSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
