5f7a343ea956cef626ee00a350050d8e4a2f7a73
rogeraguiar0 <aguiarroger02@gmail.com>
2023-02-13 20:26:19 -0300

    hotfix: gerate migrations and match username max_length

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
+from rest_framework import serializers, validators
+# from rest_framework.validators import UniqueValidator
 
 from .models import User
 
@@ -7,11 +7,11 @@ class UserSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     username = serializers.CharField(
         max_length=150,
-        validators=[UniqueValidator(queryset=User.objects.all(), message="username already taken.")]
+        validators=[validators.UniqueValidator(queryset=User.objects.all(), message="username already taken.")]
     )
     email = serializers.EmailField(
         max_length=127,
-        validators=[UniqueValidator(queryset=User.objects.all(), message="email already registered.")]
+        validators=[validators.UniqueValidator(queryset=User.objects.all(), message="email already registered.")]
     )
     password = serializers.CharField(max_length=127, write_only=True)
     first_name = serializers.CharField(max_length=50)
