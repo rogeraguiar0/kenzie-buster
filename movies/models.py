ec9f7cf7c6feab2d79169ee685b4d6de94895b03
rogeraguiar0 <aguiarroger02@gmail.com>
2023-02-21 16:40:54 -0300

    feat: movie order routes

--- movies/models.py
+++ movies/models.py
@@ -21,3 +21,11 @@ class Movie(models.Model):
     user = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="movies"
     )
+
+class MovieOrder(models.Model):
+    buyed_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    movie = models.ForeignKey("movies.Movie", on_delete=models.CASCADE)
+
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
